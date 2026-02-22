eef56560fae75bfb828867560e9e430b5680035b
Vivek Sawji <168280120+vivekx11@users.noreply.github.com>
2026-02-22 15:05:05 +0530

    Add Variables basic Java program

---
+++ Basics/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        // Variables and Data Types
+        int age = 25;
+        double salary = 50000.50;
+        String name = "Vivek";
+        boolean isJavaFun = true;
+        char grade = 'A';
+        
+        // Print variables
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Salary: " + salary);
+        System.out.println("Grade: " + grade);
+        System.out.println("Is Java Fun? " + isJavaFun);
+    }
+}
