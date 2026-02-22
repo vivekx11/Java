4ae4c955e81cf61d9a9c70bab79f767df609ad3a
Vivek Sawji <168280120+vivekx11@users.noreply.github.com>
2026-02-22 15:05:39 +0530

    Add IfElse basic Java program

---
+++ Basics/IfElse.java
@@ -0,0 +1,26 @@
+public class IfElse {
+    public static void main(String[] args) {
+        // If-Else Example
+        int marks = 75;
+        
+        if (marks >= 90) {
+            System.out.println("Grade: A");
+        } else if (marks >= 80) {
+            System.out.println("Grade: B");
+        } else if (marks >= 70) {
+            System.out.println("Grade: C");
+        } else if (marks >= 60) {
+            System.out.println("Grade: D");
+        } else {
+            System.out.println("Grade: F - Fail");
+        }
+        
+        // Check if number is even or odd
+        int num = 10;
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
